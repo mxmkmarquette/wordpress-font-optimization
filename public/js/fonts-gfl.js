690038fc727084f03efc9112e05ea5012beec35e
optimalisatie <info@optimalisatie.nl>
2018-03-11 21:03:31 +0100

    core update

--- public/js/fonts-gfl.js
+++ public/js/fonts-gfl.js
@@ -1,1 +1,1 @@
-function Wb(a){return a?a:!1}var dc=z[5]?z[5][2]:0,ec=dc?dc[4]:!1,fc=dc?dc[5]:!1;!fc||fc instanceof Array||(fc=[fc]);B.fonts=function(a){function c(){window.WebFont.load(a)}25!==ec?c():O(fc,c)};
+function $b(a){return a?a:!1}var hc=z[5]?z[5][2]:0,ic=hc?hc[4]:!1,jc=hc?hc[5]:!1;!jc||jc instanceof Array||(jc=[jc]);B.fonts=function(a){function c(){window.WebFont.load(a)}25!==ic?c():O(jc,c)};
